1ff3fae4b74913ef6618f01f776fe3c9bd17b8ee
Konrad Wojewódzki <45183401+kwojewodzki@users.noreply.github.com>
2023-10-07 19:03:49 +0200

    added test for login

--- thumbnailGenerator/my_auth/tests.py
+++ thumbnailGenerator/my_auth/tests.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
 
 # Create your tests here.
+
+class LoginUserViewTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='username', password='Pas@w0rd')
+
+    def test_login(self):
+        data = {
+            'username': 'username',
+            'password': 'Pas@w0rd'
+        }
+        response = self.client.post('/login/', data)
+        self.assertEquals(response.status_code, status.HTTP_202_ACCEPTED)
--- thumbnailGenerator/thumbnailGenerator/urls.py
+++ thumbnailGenerator/thumbnailGenerator/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('my_auth.urls'))
+    path('', include('my_auth.urls'))
 ]
