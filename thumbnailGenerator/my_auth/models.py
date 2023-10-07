b2ff6f5fb45f61e45ff23c534dda45fa9d7d3e96
Konrad Wojewódzki <45183401+kwojewodzki@users.noreply.github.com>
2023-10-07 19:41:37 +0200

    added custom user model and tests

--- thumbnailGenerator/my_auth/models.py
+++ thumbnailGenerator/my_auth/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    BASIC = "BA"
+    PREMIUM = "PR"
+    ENTERPRISE = "EN"
+    TIER_CHOICES = [(BASIC, "Basic"),
+                    (PREMIUM, "Premium"),
+                    (ENTERPRISE, "Enterprise")]
+    tier = models.CharField(max_length=16, choices=TIER_CHOICES)
--- thumbnailGenerator/my_auth/tests.py
+++ thumbnailGenerator/my_auth/tests.py
@@ -1,11 +1,49 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .models import User
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    def test_create_basic_user(self):
+        user = User.objects.create_user(username='username', password='Pas@w0rd', tier='BA')
+        data = {
+            'username': 'username',
+            'password': 'Pas@w0rd',
+            'tier': 'BA'
+        }
+        self.assertEquals(user.username, data['username'])
+        self.assertEquals(user.tier, data['tier'])
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_create_premium_user(self):
+        user = User.objects.create_user(username='username', password='Pas@w0rd', tier='PR')
+        data = {
+            'username': 'username',
+            'password': 'Pas@w0rd',
+            'tier': 'PR'
+        }
+        self.assertEquals(user.username, data['username'])
+        self.assertEquals(user.tier, data['tier'])
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_create_enterprise_user(self):
+        user = User.objects.create_user(username='username', password='Pas@w0rd', tier='EN')
+        data = {
+            'username': 'username',
+            'password': 'Pas@w0rd',
+            'tier': 'EN'
+        }
+        self.assertEquals(user.username, data['username'])
+        self.assertEquals(user.tier, data['tier'])
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
 
 class LoginUserViewTest(APITestCase):
 
